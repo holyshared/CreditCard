fb4f5a7d74a99226e655f1d1acc4fee83a11beaa
holyshared <holy.shared.design@gmail.com>
2017-05-14 21:04:06 +0900

    update indent to space

--- CreditCardTest/Test.cs
+++ CreditCardTest/Test.cs
@@ -1,5 +1,5 @@
-﻿using NUnit.Framework;
-using CreditCard;
+﻿using CreditCard;
+using NUnit.Framework;
 
 namespace CreditCard.UnitTest
 {
@@ -7,40 +7,40 @@ namespace CreditCard.UnitTest
     public class Test
     {
 
-		[Test()]
-		public void TestVISAMatches()
-		{
-            Assert.IsTrue(CreditCard.VISA.matches("411111********11"));
+        [Test()]
+        public void TestVISAMatches()
+        {
+            Assert.IsTrue(VISA.matches("411111********11"));
         }
 
         [Test()]
-		public void TestMasterCardMatches()
-		{
-            Assert.IsTrue(CreditCard.MasterCard.matches("555555********44"));
-		}
+        public void TestMasterCardMatches()
+        {
+            Assert.IsTrue(MasterCard.matches("555555********44"));
+        }
 
-		[Test()]
-		public void TestAmericanExpressMatches()
-		{
-			Assert.IsTrue(CreditCard.AmericanExpress.matches("378282*******05"));
-			Assert.IsTrue(CreditCard.AmericanExpress.matches("348282*******05"));
-		}
+        [Test()]
+        public void TestAmericanExpressMatches()
+        {
+            Assert.IsTrue(AmericanExpress.matches("378282*******05"));
+            Assert.IsTrue(AmericanExpress.matches("348282*******05"));
+        }
 
-		[Test()]
-		public void TestJCBMatches()
-		{
-            Assert.IsTrue(CreditCard.JCB.matches("353011********00"));
-		}
+        [Test()]
+        public void TestJCBMatches()
+        {
+            Assert.IsTrue(JCB.matches("353011********00"));
+        }
 
-		[Test()]
-		public void TestDinersClubMatches()
-		{
-			Assert.IsTrue(CreditCard.DinersClub.matches("300693******04"));
-			Assert.IsTrue(CreditCard.DinersClub.matches("305693******04"));
-			Assert.IsTrue(CreditCard.DinersClub.matches("309593******04"));
-			Assert.IsTrue(CreditCard.DinersClub.matches("365693******04"));
-			Assert.IsTrue(CreditCard.DinersClub.matches("385693******04"));
-			Assert.IsTrue(CreditCard.DinersClub.matches("395693******04"));
+        [Test()]
+        public void TestDinersClubMatches()
+        {
+            Assert.IsTrue(DinersClub.matches("300693******04"));
+            Assert.IsTrue(DinersClub.matches("305693******04"));
+            Assert.IsTrue(DinersClub.matches("309593******04"));
+            Assert.IsTrue(DinersClub.matches("365693******04"));
+            Assert.IsTrue(DinersClub.matches("385693******04"));
+            Assert.IsTrue(DinersClub.matches("395693******04"));
         }
     }
 }
