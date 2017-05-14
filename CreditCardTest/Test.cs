66445fb5b73e29d3639fe433a6a0ff456c37c935
holyshared <holy.shared.design@gmail.com>
2017-05-14 20:10:28 +0900

    fix test

--- CreditCardTest/Test.cs
+++ CreditCardTest/Test.cs
@@ -10,13 +10,20 @@ namespace CreditCard.UnitTest
 		[Test()]
 		public void TestVISAMatches()
 		{
-            Assert.IsTrue(CreditCard.VISA.matches("4***************"));
-		}
+            Assert.IsTrue(CreditCard.VISA.matches("411111********11"));
+        }
 
         [Test()]
 		public void TestMasterCardMatches()
 		{
-            Assert.IsTrue(CreditCard.MasterCard.matches("5***************"));
+            Assert.IsTrue(CreditCard.MasterCard.matches("555555********44"));
+		}
+
+		[Test()]
+		public void TestAmericanExpressMatches()
+		{
+			Assert.IsTrue(CreditCard.AmericanExpress.matches("378282*******05"));
+			Assert.IsTrue(CreditCard.AmericanExpress.matches("348282*******05"));
 		}
 	}
 }
