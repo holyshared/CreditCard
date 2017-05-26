3ffb1f3c2dc85c7ca0c11507572429a54f56ae11
holyshared <holy.shared.design@gmail.com>
2017-05-26 13:30:56 +0900

    add detect methods

--- CreditCard/Brand.fs
+++ CreditCard/Brand.fs
@@ -18,3 +18,12 @@ module Brand =
   let AmericanExpress = MakeCardBrand "Amex" [StartsWithOne ["34"; "37"]]
   let JCB = MakeCardBrand "JCB" [RangeOfDigits (NumberRange(3528, 3589), 4)]
   let DinersClub = MakeCardBrand "Diners Club" [ (StartsWithOne ["3095"; "36"]); (RangeOfDigitsOne [(NumberRange(300, 305), 3); (NumberRange(38, 39), 2)]) ]
+
+  let Detect (s: CardNumber) =
+    let brands = [VISA; MasterCard; AmericanExpress; JCB; DinersClub]
+    let rec detect (brands: IBrand list) =
+      match brands with
+        | [] -> false
+        | hd::tail ->
+          if hd.Matches(s) then true else detect tail
+    detect brands
