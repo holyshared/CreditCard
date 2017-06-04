f6a6bacfbc7bdd134760d5869be7facb059c9ad0
holyshared <holy.shared.design@gmail.com>
2017-06-04 15:59:19 +0900

    v0.2.1

--- CreditCard/AssemblyInfo.fs
+++ CreditCard/AssemblyInfo.fs
@@ -9,8 +9,8 @@ open System.Reflection
 [<assembly: AssemblyCopyright("${AuthorCopyright}")>]
 [<assembly: AssemblyTrademark("")>]
 
-[<assembly: AssemblyVersion("0.2.0.*")>]
-[<assembly: AssemblyFileVersion("0.2.0.*")>]
+[<assembly: AssemblyVersion("0.2.1.*")>]
+[<assembly: AssemblyFileVersion("0.2.1.*")>]
 
 //[<assembly: AssemblyDelaySign(false)>]
 //[<assembly: AssemblyKeyFile("")>]
